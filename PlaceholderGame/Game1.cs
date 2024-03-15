b78599daf64bfcb949539fc63953ad27a87c1019
Thomas Altenburger <thomas@flying-oak.com>
2024-03-15 11:41:54 +0100

    Code push

---
+++ PlaceholderGame/Game1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PlaceholderGame;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private Texture2D _emptyTexture;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+        Window.AllowUserResizing = false;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // TODO: use this.Content to load your game content here
+
+        _emptyTexture = Content.Load<Texture2D>("empty");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(_emptyTexture, Vector2.Zero, Color.White);
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ PlaceholderGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PlaceholderGame
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
